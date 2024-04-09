f19f77df0465ca3f65d7882a62d625904ccefcab
dengbin <dengcheez@gmail.com>
2024-04-09 19:04:53 +0800

    mock

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,22 +1,59 @@
 <template>
- 
+
   <div class="page">
     <el-button type="primary" size="default" :icon="Plus">点击</el-button>
     <el-button type="primary" size="small" :icon="Edit">点击</el-button>
 
-     <SvgIcon name="phone"></SvgIcon>
+    <SvgIcon name="phone"></SvgIcon>
 
   </div>
 </template>
 
 <script setup lang="ts">
-import {Plus, Edit} from "@element-plus/icons-vue"
-console.log(import.meta.env);
+import { Plus, Edit } from "@element-plus/icons-vue";
+import { onMounted } from "vue";
+import request from "./utils/request";
+// import axios from 'axios';
+// console.log(import.meta.env);
+
+onMounted(() => {
+  console.log('onMounted');
+  request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111'
+    }
+  }).then((res) => {
+    console.log('res ', res);
+
+  });
+
+});
+
+// function login() {
+  // request({
+  //   url: '/user/login',
+  //   method: 'post',
+  //   data: {
+  //     username: 'admin',
+  //     password: '111111'
+  //   }
+  // }).then((res) => {
+  //   console.log('res ', res);
+
+  // });
+  
+// }
+
+
+
+
 </script>
 
 <style lang="scss" scoped>
-
- .page {
+.page {
   background-color: $base-color;
- }
+}
 </style>
