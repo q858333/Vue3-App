22566f458135605b31e46c61fba4572aa7c7ac51
dengbin <dengcheez@gmail.com>
2024-04-25 15:34:49 +0800

    品牌页ui

--- vite-project/src/views/product/trademark/index.vue
+++ vite-project/src/views/product/trademark/index.vue
@@ -1,14 +1,44 @@
 <template>
-    <div>
-        <h1>品牌管理</h1>
+    <el-card>
+        <el-button type="primary" icon="Plus">添加品牌</el-button>
 
-    </div>
+        <el-table style="margin: 10px 0px" border>
+            <el-table-column label="序号" width="100" />
+            <el-table-column label="品牌名称" />
+            <el-table-column label="品牌LOGO" />
+            <el-table-column label="品牌操作" />
+        </el-table>
+
+        <el-pagination
+      v-model:current-page="currentPage"
+      v-model:page-size="pageSize"
+      :page-sizes="[3, 6, 9]"
+      :background="true"
+      layout="prev, pager, next ,->, total, sizes,jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+    </el-card>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+let currentPage = ref(1);
+
+let pageSize = ref(3);
+
+let total = ref(100);
+
+function handleSizeChange (){
+
+}
+
+function handleCurrentChange (){
+    
+}
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
